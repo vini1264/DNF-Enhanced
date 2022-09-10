4e26c2858a1920358e2c9e90125b1b132fa625bb
vini1264 <52476409+vini1264@users.noreply.github.com>
2022-09-09 22:40:07 -0400

    Update setup.bat

--- System/setup.bat
+++ System/setup.bat
@@ -3,6 +3,7 @@ mpkgx MegaPackage.dat Temp
 del Temp\System\Engine.u
 del Temp\System\Editor.u
 del Temp\System\dnWindow.u
+del Temp\System\dnGame.u
 xcopy Temp\* ..\ /s /y
 rmdir Temp /s /q
 cd shaders
