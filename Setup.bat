ddeff5cf7a7df1509b9be906512bbef4a5fa6139
vini1264 <52476409+vini1264@users.noreply.github.com>
2022-10-12 18:35:04 -0400

    Add the Vanilla HUD in the main repo

--- Setup.bat
+++ Setup.bat
@@ -14,27 +14,50 @@ cd ..
 cd ..
 cls
 
-:start
+:vulkanstart
 SET choice=
 SET /p choice=Do you want to install the Vulkan wrapper? [Y] [N]: 
 IF NOT '%choice%'=='' SET choice=%choice:~0,1%
-IF '%choice%'=='Y' GOTO yes
-IF '%choice%'=='y' GOTO yes
-IF '%choice%'=='N' GOTO no
-IF '%choice%'=='n' GOTO no
-IF '%choice%'=='' GOTO no
+IF '%choice%'=='Y' GOTO vulkanyes
+IF '%choice%'=='y' GOTO vulkanyes
+IF '%choice%'=='N' GOTO vulkanno
+IF '%choice%'=='n' GOTO vulkanno
+IF '%choice%'=='' GOTO vulkanno
 ECHO "%choice%" is not valid
 ECHO.
-GOTO start
+GOTO vulkanstart
 
-:no
+:vulkanno
+cls
+GOTO hudstart
+
+:vulkanyes
+xcopy DXVK\* System /y
+cls
+GOTO hudstart
+
+:hudstart
+SET choice=
+SET /p choice=Do you want to install the alternative HUD? [Y] [N]: 
+IF NOT '%choice%'=='' SET choice=%choice:~0,1%
+IF '%choice%'=='Y' GOTO hudyes
+IF '%choice%'=='y' GOTO hudyes
+IF '%choice%'=='N' GOTO hudno
+IF '%choice%'=='n' GOTO hudno
+IF '%choice%'=='' GOTO hudno
+ECHO "%choice%" is not valid
+ECHO.
+GOTO hudstart
+
+:hudno
+xcopy OldHUD\* System /y
 cls
 ECHO DNF: Enhanced was successfully installed
 PAUSE
 EXIT
 
-:yes
-xcopy DXVK\* System /y
+:hudyes
+xcopy AltHUD\* System /y
 cls
 ECHO DNF: Enhanced was successfully installed
 PAUSE
